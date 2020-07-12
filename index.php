c143c68093338850081d7d4ec0ed4475c5ea0a5c
Gerardo Chaico <chaicopadillag@gmail.com>
2020-07-12 18:37:23 -0500

    Add start apache in arch index

--- index.php
+++ index.php
@@ -13,4 +13,9 @@ require_once "controler/plantillaControler.php";
 
 $plantillaControl = new PlantillaControler();
 $plantillaControl->urls();
-//FIXME: php install=> apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline
+/*
+TODO: iniciar apache arch sudo systemctl restart httpd
+ *
+ *
+FIXME: php install=> apt install php7.4 libapache2-mod-php7.4 php7.4-mysql php-common php7.4-cli php7.4-common php7.4-json php7.4-opcache php7.4-readline
+ */
